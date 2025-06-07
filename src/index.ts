7ff742b6a184ab54ac02a257460c52668cea4223
Shane <j8yeschph@mozmail.com>
2025-06-07 09:00:53 -0700

    Fixed non-dev dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import { greet } from '@utils/greeter';
 
+import { blake2b } from '@noble/hashes/blake2';
+
 const message: String = greet('World');
 console.log(message);
 
 console.log('Environment Variables:');
 console.log('-----------------------');
 console.log(`NODE_ENV: ${process.env['SUPER_SECRETIVE_SECRET']}`);
+
+const hash: string = Buffer.from(blake2b('Hello, World!', { dkLen: 32 })).toString('hex');
+
+console.log('Hash:', hash);
