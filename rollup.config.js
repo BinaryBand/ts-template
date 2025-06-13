acf900134efc4156b10f97615b438ac0b0a49fe9
BinaryBand <g-maf.backpedal641@passinbox.com>
2025-06-12 20:13:31 -0700

    Migrate to esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,18 @@
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 export default {
   input: 'src/index.ts',
-  output: { file: 'dist/bundle.js', format: 'cjs' },
+  output: { file: 'dist/bundle.js', format: 'esm' },
   plugins: [
     commonjs(),
     json(),
-    resolve({ preferBuiltins: true }),
-    terser({
-      compress: { drop_console: true },
-      format: { comments: false },
-    }),
+    nodeResolve({ preferBuiltins: true }),
+    terser({ format: { comments: false } }),
     typescript(),
     typescriptPaths(),
   ],
