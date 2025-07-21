4c5682d6fa9d7e16a4c9bd21ff705665a7c5cd3d
BinaryBand <BinaryBand@users.noreply.github.com>
2025-07-20 20:09:57 -0700

    Added types

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,24 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: { file: 'dist/bundle.js', format: 'esm' },
-  plugins: [
-    commonjs(),
-    json(),
-    nodeResolve({ preferBuiltins: true }),
-    terser({ format: { comments: false } }),
-    typescript(),
-    typescriptPaths(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: { file: 'dist/index.js', format: 'cjs' },
+    plugins: [
+      commonjs(),
+      json(),
+      nodeResolve({ preferBuiltins: true }),
+      terser({ format: { comments: false } }),
+      typescript(),
+      typescriptPaths(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), typescript(), typescriptPaths()],
+  },
+];
